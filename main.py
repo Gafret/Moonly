018365c406fcb426a205a49bd53a32ebf74d3d1f
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-02 20:40:50 +0300

    feat: non-determenistic approach sketch

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
 import datetime
 
 from dotenv import load_dotenv
+from langchain_core.messages import RemoveMessage
 from langchain_core.tools import tool
 from langgraph.prebuilt import create_react_agent
+from zoneinfo import ZoneInfo
 
 load_dotenv()
 
 
-@tool(return_direct=True)
+def delete_excessive_messages(state):
+    messages = state["messages"]
+    if len(messages) > 5:
+        return {"messages": [RemoveMessage(id=m.id) for m in messages[:5]]}
+
+
+@tool(parse_docstring=True)
 def get_current_time() -> dict:
-    """Return the current UTC time in ISO‑8601 format.
-    Example → {"utc": "2025‑05‑21T06:42:00Z"}"""
+    """Return both UTC and Europe/Amsterdam full time and just hours."""
 
-    return {"utc": str(datetime.datetime.now())}
+    local = datetime.datetime.now(tz=ZoneInfo("Europe/Amsterdam"))
+    utc = datetime.datetime.now(datetime.timezone.utc)
+
+    return {
+        "utc_time": utc,
+        "utc_hour": utc.hour,
+        "local": local,
+        "local_hour": local.hour,
+    }
 
 
-tools = [get_current_time]
+@tool(parse_docstring=True)
+def recommend_refreshment(hour: int) -> dict:
+    """Based on current local hour gotten from get_current_time decides what user will eat and drink.
+
+    Args:
+        hour: local hour.
+    """
+
+    if hour < 17:
+        return {"drink": "coffee", "snack": "cookies"}
+    else:
+        return {"drink": "kefir", "snack": "cookies"}
+
+
+tools = [get_current_time, recommend_refreshment]
 
 graph = create_react_agent(
     model="google_genai:gemini-2.0-flash",
     tools=tools,
+    post_model_hook=delete_excessive_messages,
 )
